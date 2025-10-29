b8c4fea25c03c690986cef9b6fdecd4a8e43e162
ABHINAYAKRISHNA <abhinaykrishnanaralasetti@gmail.com>
2025-10-29 20:53:13 +0530

    Time: 16 ms (66.06%), Space: 17.8 MB (52.72%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,12 +5,11 @@ class Solution:
         r=0
         map =set()
         while l<len(s) and r<len(s):
-            print("--",map)
             while s[r] in map and r<len(s) and l<len(s):
                 map.remove(s[l])
                 l+=1
             map.add(s[r])
-            res=max(len(map),res)
+            res=max(r-l+1,res)
             r+=1
         return res
 
